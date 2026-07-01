8997bb03b13e9e947e24e729dda4c2a89524c773
Angad <angadsankhla09@gmail.com>
2026-07-01 18:57:34 +0530

    added rust, c++, python v1

--- worker/index.ts
+++ worker/index.ts
@@ -8,7 +8,6 @@ const client = createClient()
 client.connect()
   .then(async () => {
     while (1) {
-      console.log("running users code")
       const response = await client.rPop("problems")
       if (!response) {
         await new Promise((r) => setTimeout(r, 1000))
@@ -28,9 +27,45 @@ client.connect()
         response.stdout.on("data", (chunk) => {
           console.log(chunk.toString())
         })
-
-
       }
+
+      if (language === "py") {
+        console.log("running python code")
+        await new Promise((r) => setTimeout(r, 1000))
+        const filePath = __dirname + "/code/a.py"
+        fs.writeFileSync(filePath, code)
+        const response = spawn("python3", [filePath])
+        response.stdout.on("data", (chunk) => {
+          console.log(chunk.toString())
+        })
+      }
+
+      if (language === "cpp") {
+        console.log("running c++ code")
+        const filePath = __dirname + "/code/a.cpp"
+        fs.writeFileSync(filePath, code)
+        spawn("g++", [filePath, "-o", "./code/out"])
+        await new Promise((r) => setTimeout(r, 3000))
+        const response = spawn("./code/out")
+        response.stdout.on("data", (chunk) => {
+          console.log(chunk.toString())
+        })
+      }
+
+      if (language === "rust") {
+        console.log("running rust code")
+        const filePath = __dirname + "/code/a.rs"
+        fs.writeFileSync(filePath, code)
+        spawn("rustc", [filePath, "-o", "./code/rs-out"])
+        await new Promise((r) => setTimeout(r, 3000))
+        const response = spawn("./code/rs-out")
+        response.stdout.on("data", (chunk) => {
+          console.log(chunk.toString())
+        })
+      }
+
+
+
 
     }
   })
